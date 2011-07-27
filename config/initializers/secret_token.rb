6778c73ef19da2620f9cc3c58afa1a66785d062d
Jonathan Gottfried <jonmarkgo@gmail.com>
2011-07-27 15:41:51 -0400

    Initial rails repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClientRoulette::Application.config.secret_token = '3a607d578b3fad36e0ac18b899389084740d72ac6b52570d3df5c111540a7053944433950343ee58fba34622707d124af67bd30cc29de8b3eb7b1bf6df9d8876'
